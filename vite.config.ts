5f23824228d3fc962b5d6c7be59596a377500c05
nashnet1 <108474989+engkunasya@users.noreply.github.com>
2024-10-19 17:24:32 +0800

    ssl

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // dev mode for prod
+  server: {
+    host: '0.0.0.0',  // Allow access from all networks
+    port: 3000,       // You can change this port if needed
+  }
 })
